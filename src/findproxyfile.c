3b15192492de280335886ad3e2df0321e291250e
Andrew McNab <andrew.mcnab@manchester.ac.uk>
2006-06-19 09:10:39 +0000

    Delegation protocol update

--- src/findproxyfile.c
+++ src/findproxyfile.c
@@ -1,5 +1,5 @@
 /*
-   Copyright (c) 2002-4, Andrew McNab, University of Manchester
+   Copyright (c) 2002-6, Andrew McNab, University of Manchester
    All rights reserved.
 
    Redistribution and use in source and binary forms, with or
